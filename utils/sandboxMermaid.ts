e9ca5e20c117c3132fbff899de61c139cdfd7cd7
0kenx <km@nxfi.app>
2023-12-08 17:02:19 +0100

    Update sandboxMermaid.ts

--- utils/sandboxMermaid.ts
+++ utils/sandboxMermaid.ts
@@ -1,13 +1,21 @@
 import { SendMessageResult } from '@ton-community/sandbox';
+import { CommonMessageInfoInternal } from 'ton-core';
 
 function shortenAddress(addr: string): string {
     if (addr.length < 10) return addr;
     return addr.slice(0, 5) + ".." + addr.slice(-4);
 }
 
+function bigIntDisplay(key: any, value: any) {
+  if (typeof value === "bigint") {
+    return value.toString();
+  }
+  return value;
+}
+
 // Function to parse SendMessageResult
 function parseSendMessageResult(txResult: SendMessageResult) {
-  const nodes: string[] = [];
+  const nodeSet = new Set<string>();
   const edges: string[] = [];
 
   txResult.transactions.forEach((transaction) => {
@@ -20,18 +28,23 @@ function parseSendMessageResult(txResult: SendMessageResult) {
     // }
 
     if (outMessagesCount > 0) {
-      for (const { info: { src, dest }, body } of outMessages.values()) {
-        const srcNode = shortenAddress(`${src}`);
-        const destNode = shortenAddress(`${dest}`);
+      for (const { info, body } of outMessages.values()) {
+        if ("value" in info && (info as CommonMessageInfoInternal).value !== undefined) {
+          const { src, dest, value } = (info as CommonMessageInfoInternal);
+          const srcNode = shortenAddress(`${src}`);
+          const destNode = shortenAddress(`${dest}`);
 
-        nodes.push(srcNode);
-        nodes.push(destNode);
+          nodeSet.add(srcNode);
+          nodeSet.add(destNode);
 
-        edges.push(`${srcNode} ->> ${destNode}: ${body}`);
+          edges.push(`${srcNode} ->> ${destNode}: ${JSON.stringify(value, bigIntDisplay)}`);
+        }
+        
       }
     }
   });
 
+  const nodes = [...nodeSet!];
   return { nodes, edges };
 }
 
@@ -39,9 +52,10 @@ function parseSendMessageResult(txResult: SendMessageResult) {
 export function generateMermaidDiagram(txResult: SendMessageResult) {
     const { nodes, edges } = parseSendMessageResult(txResult);
     const graph = `
-    sequenceDiagram
-    ${nodes.map((node, index) => `participant ${node}`).join('\n')}
-    ${edges.map((edge, index) => `${edge}`).join('\n')}
+sequenceDiagram
+  autonumber
+  ${nodes.map((node, index) => `participant ${node}`).join('\n  ')}
+  ${edges.map((edge, index) => `${edge}`).join('\n  ')}
   `;
 
   return graph;
